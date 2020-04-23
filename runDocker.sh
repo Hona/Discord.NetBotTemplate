28c411f82dcc6540595b17a6b20f8a59aa84937b
Luke Parker <lukemparker@outlook.com>
2020-04-23 11:50:44 +1000

    Fix path in runDocker.sh

--- runDocker.sh
+++ runDocker.sh
@@ -5,7 +5,7 @@ echo '-= Removing Old template-bot-production Container =-'
 docker container rm template-bot-production
 
 echo '-= Building Docker Image from Dockerfile ='
-docker build -t template-bot -f ./src/TemplateDiscordBot/Dockerfile .
+docker build -t template-bot -f ./src/DiscordBotTemplate/Dockerfile .
 
 echo '-= Runnning the Image ='
 docker run -v $PWD/PRODUCTION:/app/PRODUCTION -v $PWD/DEVELOPMENT:/app/DEVELOPMENT --network host --restart on-failure:5 --name "template-bot-production" -d template-bot
