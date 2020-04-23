fec02e8745f9c0fc18a78078d4e1374965b22a9d
Luke Parker <lukemparker@outlook.com>
2020-04-23 11:17:11 +1000

    Create path constants which changes the folder depending on the environment

---
+++ src/DiscordBotTemplate/Constants/PathConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+namespace DiscordBotTemplate.Constants
+{
+    public static class PathConstants
+    {
+        private static string EnvironmentConfigFolder => Path.Combine(Environment.CurrentDirectory, Environment.GetEnvironmentVariable("ENVIRONMENT"), "config");
+        public static string DiscordBotTokenFile => Path.Combine(EnvironmentConfigFolder, "discordBotToken.txt");
+    }
+}
