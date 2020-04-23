6ff058ec1f3d2959308720c824beb4b2437365dd
Luke Parker <lukemparker@outlook.com>
2020-04-23 11:17:30 +1000

    Create dependency injection service which builds a service provider

---
+++ src/DiscordBotTemplate/Services/DependencyInjectionService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Discord.Commands;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DiscordBotTemplate.Services
+{
+    public class DependencyInjectionService
+    {
+        private readonly DiscordSocketClient _discordClient;
+        private readonly CommandService _baseCommandService;
+        public DependencyInjectionService(DiscordSocketClient discordClient, CommandService baseCommandService)
+        {
+            _discordClient = discordClient;
+            _baseCommandService = baseCommandService;
+        }
+
+        public IServiceProvider BuildServiceProvider() =>
+            new ServiceCollection()
+                .AddSingleton(_discordClient)
+                .AddSingleton(_baseCommandService)
+                .BuildServiceProvider();
+    }
+}
