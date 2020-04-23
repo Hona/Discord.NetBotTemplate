57729e3b54d237b16893c12e825e488cdee548a0
Luke Parker <lukemparker@outlook.com>
2020-04-23 11:19:39 +1000

    Create a string extension that will clean line endings, which have funky behaviors between Unix/Windows

---
+++ src/DiscordBotTemplate/Utilities/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DiscordBotTemplate.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string CleanLineEndings(this string input) 
+            => Regex.Replace(input.Trim(), @"\r\n?|\n", string.Empty);
+    }
+}
