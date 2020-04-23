84e2f6aa070064c79734062fdf5514d11ac92bb2
Luke Parker <lukemparker@outlook.com>
2020-04-23 11:21:14 +1000

    Create a test module that only the bot owner can use

---
+++ src/DiscordBotTemplate/Discord/Commands/OwnerModule.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace DiscordBotTemplate.Discord.Commands
+{
+    [RequireOwner]
+    public class OwnerModule : ModuleBase
+    {
+        [Command("test")]
+        public async Task TestCommandAsync(string input)
+        {
+            await ReplyAsync(input);
+        }
+    }
+}
