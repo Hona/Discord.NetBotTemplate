911352a9e0beb9227e8c0166128a62c43b831062
Luke Parker <lukemparker@outlook.com>
2020-04-23 11:18:26 +1000

    Create application helper which announces exit, then exits

---
+++ src/DiscordBotTemplate/Utilities/ApplicationHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using DiscordBotTemplate.Logging;
+
+namespace DiscordBotTemplate.Utilities
+{
+    public static class ApplicationHelper
+    {
+        public static void AnnounceAndExit()
+        {
+            Logger.LogInfo("Application closing safely...");
+            Environment.Exit(0);
+        }
+    }
+}
